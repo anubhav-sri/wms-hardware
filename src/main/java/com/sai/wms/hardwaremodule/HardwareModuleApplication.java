47a4efce7302f9cc9fa45ceb12bc2dab894e1103
anubhavsrivastava <anubhav.srivastava@n26.com>
2020-02-19 12:43:53 +0100

    Application setup for configuring the sensors and hardware in warehouse

---
+++ src/main/java/com/sai/wms/hardwaremodule/HardwareModuleApplication.java
@@ -0,0 +1,13 @@
+package com.sai.wms.hardwaremodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sai/wms/hardwaremodule/HardwareModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.wms.hardwaremodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

d00c6b7715adc59ae1d302f13fc89c5d81d3481b
anubhavsrivastava <anubhav.srivastava@n26.com>
2020-03-02 22:55:22 +0100

    handles cross origing request

--- src/main/java/com/sai/wms/hardwaremodule/controllers/TemperatureDeviceController.java
+++ src/main/java/com/sai/wms/hardwaremodule/controllers/TemperatureDeviceController.java
@@ -3,12 +3,14 @@ package com.sai.wms.hardwaremodule.controllers;
 import com.sai.wms.hardwaremodule.models.Device;
 import com.sai.wms.hardwaremodule.services.TemperatureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class TemperatureDeviceController {
     private TemperatureService temperatureService;
 
